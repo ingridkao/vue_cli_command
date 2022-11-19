716a2529345fe483bcb1ac1de4cd6a1e7c566c2e
ingrid <ingrid7613@gmail.com>
2022-11-19 12:21:08 +0800

    create component & view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld title="嘿嘿" msg="1dsadadd"/>
+    <HelloWorld title="ffsfdsd" msg="2"/>
+    <HelloWorld title="eqwdwadad" msg="3"/>
   </div>
 </template>
 
